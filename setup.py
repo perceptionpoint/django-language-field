54643c231e98e828245a0bac82a22121e2efaab4
Yonatan Bitton <yonatan.bitton@perception-point.io>
2023-09-12 12:01:13 +0300

    Remove support from Python2.7

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'Framework :: Django :: 1.9',
         'Environment :: Web Environment',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
